641b9a80ddc14ac86d42087268a4a9c803b002f2
xuxiaoyang21 <1334909533@qq.com>
2017-01-18 23:08:42 +0800

    创建ELMS租赁项目

---
+++ src/main/java/com/mxiaixy/web/HomeServlet.java
@@ -0,0 +1,19 @@
+package com.mxiaixy.web;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+/**
+ * Created by Mxia on 2017/1/18.
+ */
+@WebServlet("/home")
+public class HomeServlet extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        req.getRequestDispatcher("WEB-INF/views/home.jsp");
+    }
+}
